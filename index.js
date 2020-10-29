1de804a6b819af3e6950043563e6c0991ce115a7
rajharsh81070 <rajharsh81070@gmail.com>
2020-10-29 10:26:01 +0530

    Backend Done.

--- index.js
+++ index.js
@@ -25,61 +25,52 @@ const headers = {
   'Content-Type': 'application/json'
 };
 
-async function getId(req, callbackfunc) {
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+async function createPayid(req, callbackfunc) {
   const url = 'https://api.test.paysafe.com/paymenthub/v1/customers';
-  //body
   const data = {
     merchantCustomerId: req.email + req.firstName + req.phone,
     firstName: req.firstName,
     email: req.email,
     phone: req.phone,
   };
-  //options for post request,headersand url
   const requestOptions = {
     url: url,
     headers: headers,
     body: JSON.stringify(data),
     method: 'POST'
   };
-  //callback for requests	
-  function callback(err, res, body) {
-    // console.log(err)
-    // console.log(res)
-    // console.log(JSON.parse(body).id)
-    return callbackfunc(JSON.parse(body).id);
-  }
 
-  request(requestOptions, callback);
+  request(requestOptions, (err, res, body) => {
+    return callbackfunc(JSON.parse(body).id);
+  });
 }
 
-async function getToken(payid, callbackfunc) {
-  //url
+async function createToken(payid, callbackfunc) {
   const url = 'https://api.test.paysafe.com/paymenthub/v1/customers/';
-  //body
+
   const data = {
-    "merchantRefNum": "Ref123",
+    "merchantRefNum": uuidv4(),
     "paymentTypes": [
       "CARD"
     ]
   };
 
-  const options = {
+  const requestOptions = {
     url: url + payid + '/singleusecustomertokens',
     headers: headers,
     body: JSON.stringify(data),
     method: 'POST'
   };
-  function callback(err, res, body) {
+
+  request(requestOptions, (err, res, body) => {
     return callbackfunc(JSON.parse(body).singleUseCustomerToken);
-  }
-
-  request(options, callback);
-}
-
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
   });
 }
 
@@ -94,17 +85,16 @@ async function payment(req, callbackfunc) {
     "description": "Payment"
   }
 
-  const options = {
+  const requestOptions = {
     url: url,
     headers: headers,
     body: JSON.stringify(data),
     method: 'POST'
   };
-  function callback(error, response, body) {
-    console.log(body);
-    return callbackfunc(body);
-  }
-  request(options, callback);
+
+  request(requestOptions, (err, res, body) => {
+    return callbackfunc(JSON.parse(body));
+  });
 }
 
 app.post("/api/token", (req, res) => {
@@ -112,9 +102,8 @@ app.post("/api/token", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      console.log("User", user);
       if (!user) {
-        await getId(req.body,
+        await createPayid(req.body,
           function (result) {
             const newUser = {
               payid: result,
@@ -125,14 +114,14 @@ app.post("/api/token", (req, res) => {
                 console.log(err);
               } else {
                 user = newlyCreated;
-                await getToken(user.payid, function (result) {
+                await createToken(user.payid, function (result) {
                   res.send({ token: result });
                 })
               }
             });
           });
       } else {
-        await getToken(user.payid, function (result) {
+        await createToken(user.payid, function (result) {
           res.send({ token: result });
         })
       }
